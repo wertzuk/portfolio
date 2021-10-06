a337de27f136a7f28cdfc1c16bd9716b2bfa337a
wertzuk <pascalb95@gmx.de>
2021-10-07 00:23:25 +0200

    added grid

--- script.js
+++ script.js
@@ -10,8 +10,41 @@ closeIcon.addEventListener("click", handleClasses);
 listItems.forEach((item) => item.addEventListener("click", handleClasses));
 
 function handleClasses() {
+  console.log();
   hamburger.classList.toggle("active");
   menu.classList.toggle("active");
   closeIcon.classList.toggle("active");
+
   document.body.classList.toggle("no-scroll");
 }
+
+//
+// Animation
+//
+const dataText = ["Hi.", "Ich bin Pascal", "Frontend-Entwickler.", "Motiviert"];
+
+// function typeWriter(text, i, fnCallback) {
+//   if (i < text.length) {
+//     document.querySelector(".animated").innerHTML =
+//       text.substring(0, i + 1) + '<span aria-hidden="true"></span>';
+
+//     setTimeout(() => {
+//       typeWriter(text, i + 1, fnCallback);
+//     }, 100);
+//   } else if (typeof fnCallback == "function") {
+//     setTimeout(fnCallback, 700);
+//   }
+// }
+// function StartTextAnimation(i) {
+//   if (typeof dataText[i] == "undefined") {
+//     setTimeout(function () {
+//       StartTextAnimation(0);
+//     }, 20000);
+//   }
+//   if (i < dataText[i].length) {
+//     typeWriter(dataText[i], 0, function () {
+//       StartTextAnimation(i + 1);
+//     });
+//   }
+// }
+// StartTextAnimation(0);
